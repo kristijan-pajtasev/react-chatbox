68a4711d50257c08c770e0ddf99f21fe6cffcf98
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2017-05-21 17:33:58 +0100

    send message handler

--- src/send-message/SendMessage.js
+++ src/send-message/SendMessage.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import './send-message.css';
 
 class SendMessage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+        this.sendMessage = this.sendMessage.bind(this);
+    }
+    sendMessage() {
+        console.log(`handle ${this.message.value}`);
+    }
     render() {
         return (
             <div className="send-message">
-                <textarea></textarea>
-                <button>Send Message</button>
+                <textarea
+                    ref={(msg) => { console.log("inp"); this.message = msg; }}></textarea>
+                <button onClick={this.sendMessage}>Send Message</button>
             </div>
         );
     }
