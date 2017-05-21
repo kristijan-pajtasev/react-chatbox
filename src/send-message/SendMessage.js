121bec72964b75dbebc2f16dd795f9416e411aaf
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2017-05-21 17:15:43 +0100

    flex align

--- src/send-message/SendMessage.js
+++ src/send-message/SendMessage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import './send-message.css';
 
 class SendMessage extends Component {
     render() {
         return (
             <div className="send-message">
-
+                <textarea></textarea>
+                <button>Send Message</button>
             </div>
         );
     }
