181aa352f1fe61872a5e4747ea8625159fec68c2
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2017-05-21 17:09:52 +0100

    rename and init rendering for SendMessage

--- src/App.js
+++ src/App.js
@@ -2,19 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './messages/Messages';
-import './send-message/SendMessages';
+import SendMessage from './send-message/SendMessage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <SendMessage />
       </div>
     );
   }
---
+++ src/send-message/SendMessage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SendMessage extends Component {
+    render() {
+        return (
+            <div className="send-message">
+
+            </div>
+        );
+    }
+}
+
+export default SendMessage;
