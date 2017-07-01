dab765fd5258a11cd7a872ea1edf30105bb2ce08
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2017-07-01 17:47:24 +0100

    messages styling

--- src/components/messages/Message.js
+++ src/components/messages/Message.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
+import "./Messages.less";
 
 class Messages extends Component {
     render() {
         const { message } = this.props;
         return (
-            <li>{message}</li>
+            <li className="message">{message}</li>
         );
     }
 }
