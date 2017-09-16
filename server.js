c9a58e080bd30f2d3283725059c535d517339f0b
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2017-09-16 18:28:32 +0100

    server updated, post save and websocket

--- server.js
+++ server.js
@@ -1,6 +1,52 @@
-let express = require("express");
-let app = express();
+const express = require('express');
+const http = require('http');
+const url = require('url');
+const WebSocket = require('ws');
+const bodyParser = require('body-parser');
+let messages = [];
 
-app.listen(3000, () => {
-    console.log("listening on port 3001");
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+let socket;
+
+wss.on('connection', function connection(ws, req) {
+    socket = ws;
+    const location = url.parse(req.url, true);
+    // You might use location.query.access_token to authenticate or share sessions
+    // or req.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
+
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+    });
+
+    ws.send('something');
+});
+
+app.get('/send', function(req, res) {
+    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    socket.send('in send');
+    res.send('hello');
+});
+
+app.post('/send', function(req, res) {
+    console.log('in send');
+    console.log(req.body);
+    messages.push(req.body);
+    res.send('hello');
+});
+
+server.listen(8080, function listening() {
+    console.log('Listening on %d', server.address().port);
 });
