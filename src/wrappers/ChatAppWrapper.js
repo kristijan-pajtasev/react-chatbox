b59d7527de14cd7a345e8a5be5dba31a6094a7e6
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2017-09-17 02:31:06 +0100

    after rename fi

--- src/wrappers/ChatAppWrapper.js
+++ src/wrappers/ChatAppWrapper.js
@@ -7,7 +7,7 @@ import { bindActionCreators } from 'redux'
 class ChatAppWrapper extends React.PureComponent {
     render() {
         return (
-            <ChatApp messages={this.props.messages} saveMessage={this.props.actions.saveMessage} />);
+            <ChatApp messages={this.props.messages} sendMessage={this.props.actions.sendMessage} />);
     }
 }
 
