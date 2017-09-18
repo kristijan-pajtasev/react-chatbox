e2a09670fbf3dbcda777826f00566742c487e936
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2017-09-19 00:59:15 +0100

    redux thunk added

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from 'redux-thunk';
 import messages from '../reducers/messages';
 
-const store = createStore(combineReducers({ messages }));
+const store = createStore(combineReducers({ messages }), applyMiddleware(thunk));
 
 export default store;
