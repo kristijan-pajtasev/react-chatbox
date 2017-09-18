18fd33becfba5d0a2174f35bc6742e23c908fa82
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2017-09-19 00:52:09 +0100

    init login component added

--- src/components/ChatApp.js
+++ src/components/ChatApp.js
@@ -1,8 +1,12 @@
 import React from "react";
+
 import MessagesList from './messages/MessagesList';
 import SendMessage from './send-message/SendMessage';
+import LoginComponent from './login/Login';
+
 import MessagesService from "../services/MessagesService";
 import SocketService from '../services/SocketService';
+
 import config from '../config';
 
 class ChatApp extends React.PureComponent {
---
+++ src/components/login/Login.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class Login extends React.PureComponent {
+    render(){
+        return <div>login</div>;
+    }
+}
+export default Login;
