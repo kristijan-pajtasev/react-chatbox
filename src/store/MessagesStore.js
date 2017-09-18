8479c0d43bfa3791d314f4fee6b8ab6825447b9d
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2017-09-19 00:19:02 +0100

    messages reducer excluded

---
+++ src/store/MessagesStore.js
@@ -0,0 +1,14 @@
+import { ADD_MESSAGES, SAVE_MESSAGE } from "../constants"
+import MessagesService from '../services/MessagesService';
+
+export default (state = [], action) => {
+    switch(action.type) {
+        case ADD_MESSAGES:
+            return [...state, action.message];
+        case SAVE_MESSAGE:
+            MessagesService.sendMessage(action.message);
+            return state;
+        default:
+            return state;
+    }
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,6 @@
 import { createStore } from "redux";
-import { ADD_MESSAGES, SAVE_MESSAGE } from "../constants"
-import MessagesService from '../services/MessagesService';
+import MessagesStore from './MessagesStore';
 
-const store = createStore((state = [], action) => {
-    switch(action.type) {
-        case ADD_MESSAGES:
-            return [...state, action.message];
-        case SAVE_MESSAGE:
-            MessagesService.sendMessage(action.message);
-            return state;
-        default:
-            return state;
-    }
-});
+const store = createStore(MessagesStore);
 
 export default store;
