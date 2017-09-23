a6e2b2ba92aca22e61598e2eb557027ae35b4429
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2017-09-23 18:53:32 +0100

    username added to state and base login handler

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -2,11 +2,28 @@ import React from 'react';
 import './Login.less';
 
 class Login extends React.PureComponent {
+    constructor() {
+        super();
+        this.state = { username: '' };
+    }
+
+    handleChange(event) {
+        this.setState({username: event.target.value});
+    }
+
+    login() {
+        console.log(`login username: ${this.state.username}`);
+    }
+
     render(){
         return (
             <div className="LoginComponent">
-                <input className="username" placeholder="Enter username" type="text" />
-                <button>Login</button>
+                <input className="username"
+                       placeholder="Enter username"
+                       type="text" value={this.state.username}
+                       onChange={this.handleChange.bind(this)} />
+
+                <button onClick={this.login.bind(this)}>Login</button>
             </div>);
     }
 }
