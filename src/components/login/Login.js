05f88cfbd72ac76109be0846d6364a3bbb8b8c0c
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2017-09-23 18:48:04 +0100

    placeholder added

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -5,7 +5,7 @@ class Login extends React.PureComponent {
     render(){
         return (
             <div className="LoginComponent">
-                <input className="username" type="text" />
+                <input className="username" placeholder="Enter username" type="text" />
                 <button>Login</button>
             </div>);
     }
