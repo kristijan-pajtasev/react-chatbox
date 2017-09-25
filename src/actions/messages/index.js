0174ab9bf3e6850e26064858e2647e5112cfa3c0
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2017-09-25 02:40:35 +0100

    send messages fixed

--- src/actions/messages/index.js
+++ src/actions/messages/index.js
@@ -1,10 +1,3 @@
 import { SAVE_MESSAGE } from "../../constants";
-import MessagesService from '../../services/MessagesService';
 
-export const sendMessage = (message) => {
-    return dispatch => {
-        MessagesService.sendMessage(message).then(() => {
-            return {type: SAVE_MESSAGE, message: message};
-        })
-    }
-}
+export const sendMessage = (message) => ({type: SAVE_MESSAGE, message: message});
