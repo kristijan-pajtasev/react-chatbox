56d20142e1ed79e11d7cd06ebaabfb4e1932147c
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2017-09-25 02:41:21 +0100

    console logs removed

--- src/reducers/user/index.js
+++ src/reducers/user/index.js
@@ -1,10 +1,8 @@
 import { SET_USERNAME } from "../../constants/user"
 
 export default (state = {}, action) => {
-    console.log('action triggered' + action.type)
     switch(action.type) {
         case SET_USERNAME:
-            console.log('set username');
             return { username: action.username };
         default:
             return state;
