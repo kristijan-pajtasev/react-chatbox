212b46c83c9436ffce1d88ea2a093c1bed5bf775
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2017-10-05 01:35:25 +0100

    find user fix

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get('/message', function(req, res) {
 
 app.post('/message', function(req, res) {
     const { message } = req.body;
-    let username = users.filter(e => e.value === req.cookies.CHAT_SESSION_ID)[0].username;
+    let username = users.filter(e => e.value == req.cookies.CHAT_SESSION_ID)[0].username;
     messages.push({ message, username });
     sockets.forEach(s => {
         s.send(JSON.stringify({ type: 'MESSAGE', message, username}));
